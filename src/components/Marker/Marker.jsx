51cf3e5849b60ab708be870841ebb523eefd7c9d
a-m-dev <a.mirzaei.dev@gmail.com>
2020-10-27 03:24:43 +0330

    done

--- src/components/Marker/Marker.jsx
+++ src/components/Marker/Marker.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Marker as MAPBOX_Marker } from "react-mapbox-gl";
+import { useGlobalContext } from "containers/App/context";
 
 import "./styles.scss";
 
 const Marker = ({ markerLocation, handleActiveMarker }) => {
+  const {
+    data: { mapCenter },
+  } = useGlobalContext();
+
+  const isActivePin = useMemo(() => {
+    if (
+      mapCenter[0] === markerLocation[0] &&
+      mapCenter[1] === markerLocation[1]
+    )
+      return true;
+    else return false;
+  }, [mapCenter, markerLocation]);
+
   return (
     <MAPBOX_Marker
       coordinates={markerLocation}
@@ -12,7 +26,12 @@ const Marker = ({ markerLocation, handleActiveMarker }) => {
       onClick={() => handleActiveMarker(markerLocation)}
     >
       <div className="marker-container">
-        <i className="icon-location marker-container__icon" />
+        <i className="icon-location" />
+        <span className={`pin normal ${isActivePin && "active"}`}>
+          <span className="small-circle"></span>
+          <span className="large-circle"></span>
+          <span className="bubble"></span>
+        </span>
       </div>
     </MAPBOX_Marker>
   );
